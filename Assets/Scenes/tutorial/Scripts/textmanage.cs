229d6c4d65e861934647b4199cb5e41c26b8a84b
juyounginpark <0620ffff@naver.com>
2025-08-07 18:23:35 +0900

    Scene/settings (#6)
    * dd
    
    * 아무래도저는디자인은아닌것갓습미다
    
    * del: mesh
    
    ---------
    
    Co-authored-by: Suakim0811 <suak0811@gmail.com>

---
+++ Assets/Scenes/tutorial/Scripts/textmanage.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TutorialtextManager : MonoBehaviour
+{
+    public TextMeshProUGUI tutorialText;
+    public string[] tutorialMessages;
+    private int currentIndex = 0;
+
+    public Button prevButton;
+    public Button nextButton;
+
+    void Start()
+    {
+        ShowCurrentMessage();
+        prevButton.onClick.AddListener(ShowPrevMessage);
+        nextButton.onClick.AddListener(OnNextButtonClick);
+    }
+
+    void ShowCurrentMessage()
+    {
+        if (currentIndex >= 0 && currentIndex < tutorialMessages.Length)
+            tutorialText.text = tutorialMessages[currentIndex];
+
+        prevButton.interactable = currentIndex > 0;
+    }
+
+    public void OnNextButtonClick()
+    {
+        if (currentIndex < tutorialMessages.Length - 1)
+        {
+            currentIndex++;
+            ShowCurrentMessage();
+        }
+        else
+        {
+            tutorialText.gameObject.SetActive(false);
+            prevButton.gameObject.SetActive(false);
+            nextButton.gameObject.SetActive(false);
+        }
+    }
+
+    public void ShowPrevMessage()
+    {
+        if (currentIndex > 0)
+        {
+            currentIndex--;
+            ShowCurrentMessage();
+        }
+    }
+}
